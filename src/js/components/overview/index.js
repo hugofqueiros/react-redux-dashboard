b7adbc57a7bf6dd9c9c2a950eb369198825e5b1e
Hugo Queiros <hugofqueiros@gmail.com>
2017-01-20 16:26:06 +0100

    refactor(components): overview: updating styles

--- src/js/components/overview/index.js
+++ src/js/components/overview/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Card from '../card'
 
 import './Overview.scss';
 
 class Overview extends React.Component {
     render() {
         return (
-            <div className="Overview">
-                Overview
+            <div className="Overview container-fluid">
+                <div className="row">
+                    <Card classes="col-xs-12 col-sm-6 col-md-6 col-lg-3"/>
+                    <Card classes="col-xs-12 col-sm-6 col-md-6 col-lg-3"/>
+                    <Card classes="col-xs-12 col-sm-6 col-md-6 col-lg-3"/>
+                    <Card classes="col-xs-12 col-sm-6 col-md-6 col-lg-3"/>
+                    <Card classes="col-xs-12 col-sm-6 col-md-6 col-lg-3"/>
+                    <Card classes="col-xs-12 col-sm-6 col-md-6 col-lg-3"/>
+                    <Card classes="col-xs-12 col-sm-6 col-md-6 col-lg-3"/>
+                </div>
             </div>
         );
     }
