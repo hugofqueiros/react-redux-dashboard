7fee9fea5b69499fed464fe8bc6c39d2520afbbd
Hugo Queiros <hugofqueiros@gmail.com>
2017-01-20 16:25:35 +0100

    refactor(components): card: updating

--- src/js/components/card/index.js
+++ src/js/components/card/index.js
@@ -1,24 +1,45 @@
 import React, { PropTypes } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
 
 import './Card.scss';
 
-class Card extends React.component {
+class Card extends React.Component {
     constructor(props) {
         super(props);
+        console.log('card props', props);
     }
 
     render() {
         return(
-            <div className="Card">
-                <div className="Card-header">Header</div>
-                <div className="Card-body">Body</div>
-                <div className="Card-footer">Footer</div>
+            <div className={this.props.classes}>
+            <div className="Card" elevation="1">
+                <div className="Card-header">
+                    <div className="Card-title">This is my header</div>
+                    <div className="Card-icon"></div>
+                </div>
+                <div className="Card-body">Body like this woooo</div>
+                <div className="Card-footer">wowowoow foooooottterrr</div>
+            </div>
             </div>
         )
     }
+
+
 }
+
+Card.propTypes = {
+    hasHeader: PropTypes.bool,
+    hasFooter: PropTypes.bool,
+    icon: PropTypes.string,
+    type: PropTypes.string,
+    classes: PropTypes.string,
+};
+
+Card.defaultProps = {
+    hasHeader: true,
+    hasFooter: false,
+    icon: 'street-view',
+    type: '',
+    classes: PropTypes.string
+};
 
 export default Card;
