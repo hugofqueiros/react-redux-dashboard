c48a8000e7eb3d337d057db09bda6c3df2d18585
Hugo Queiros <hugofqueiros@gmail.com>
2017-01-30 20:50:53 +0100

    feat(redux): actions: user

---
+++ src/js/redux/actions/user.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import actionType from './actionTypes';
+
+const URL_ROOT_USER = 'https://api.github.com/users/hugofqueiros';
+
+export function fetchUser(dispatch) {
+    fetch(dispatch);
+    return {
+        type: actionType.FETCH_USER
+    }
+}
+
+function fetch(dispatch) {
+    axios({
+        method: 'get',
+        url: `${URL_ROOT_USER}`
+    }).then(function(result) {
+        dispatch(fetchUserSuccess(result));
+    }).catch(function(error) {
+        dispatch(fetchUserFailure(error))
+    });
+}
+
+function fetchUserSuccess(result) {
+    return {
+        type: actionType.FETCH_USER_SUCCESS,
+        payload: result
+    }
+}
+
+function fetchUserFailure(err) {
+    return {
+        type: actionType.FETCH_USER_FAILURE,
+        payload: err
+    }
+}
+
+
