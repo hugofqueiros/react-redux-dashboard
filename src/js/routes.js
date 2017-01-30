f4789e94cd0cf6785f8407f35e605bcc7d497baf
Hugo Queiros <hugofqueiros@gmail.com>
2017-01-30 20:55:58 +0100

    feat(js): adding routes

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './containers/App';
-import Home from './components/home';
-import Overview from './components/overview';
-import Panel from './components/panel/Panel';
+import Home from './containers/home/Home';
+import Overview from './containers/overview/Overview';
+import Visits from './containers/visits/Visits';
+import Charts from './containers/charts/Charts';
+import Maps from './containers/maps/Maps';
 
 import NotFoundPage from './components/NotFound';
 
@@ -12,7 +14,9 @@ export default (
     <Route path='/' component={App}>
         <IndexRoute component={Home} />
         <Route path="/overview" component={Overview} />
-        <Route path="/visits" component={Panel} />
+        <Route path="/visits" component={Visits} />
+        <Route path="/charts" component={Charts} />
+        <Route path="/maps" component={Maps} />
         <Route path="*" component={NotFoundPage} />
     </Route>
 );
