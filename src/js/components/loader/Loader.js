ff1fde601a10749f7ee75d25ab8d3b3b688f7415
Hugo Queiros <hugofqueiros@gmail.com>
2017-01-30 20:54:20 +0100

    feat(componets): loader

---
+++ src/js/components/loader/Loader.js
@@ -0,0 +1,50 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+
+import './Loader.scss';
+
+const mapStateToProps = ({common}) => {
+    return {
+    }
+};
+
+const defaultStyles = {
+    spinnerContainer: {
+
+    },
+    path: {
+        stokeWidth: '4'
+    }
+};
+
+
+const Loader = (props) => {
+
+    let spinnerContainerStyle = {
+        width: props.width + 'px',
+        height: props.height + 'px'
+    };
+
+    spinnerContainerStyle = {...defaultStyles.spinnerContainer, spinnerContainerStyle};
+
+    return (
+        <div className="Loader">
+            <svg className="spinner-container" viewBox="0 0 44 44" style={spinnerContainerStyle}>
+                <circle className="path" cx="22" cy="22" r="20" fill="none">
+                </circle>
+            </svg>
+        </div>
+    );
+};
+
+Loader.propTypes = {
+    width: PropTypes.number,
+    height: PropTypes.number
+};
+
+Loader.defaultProps = {
+    width: 65,
+    height: 65
+};
+
+export default connect(mapStateToProps, {})(Loader);
