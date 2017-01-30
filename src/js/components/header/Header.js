32f48be25b0feedb4c9e3c86e8915e1c3b535234
Hugo Queiros <hugofqueiros@gmail.com>
2017-01-30 20:27:11 +0100

    refactor(components): header

--- src/js/components/header/Header.js
+++ src/js/components/header/Header.js
@@ -2,55 +2,67 @@ import React, {PropTypes} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
+import {Link} from 'react-router';
 import * as UiActionsCreator from '../../redux/actions/ui';
+import * as UserActionsCreator from '../../redux/actions/user';
 
-import DatePicker from '../datepicker/';
+import DatePicker from '../datepicker/DatePicker';
 
 import './Header.scss';
 
-const mapStateToProps = ({ui}, {params}) => {
+const mapStateToProps = ({ui, user}, {params}) => {
     return {
         sidebarOpen: ui.sidebarOpen,
-        searchBarOpen: ui.searchBarOpen
+        user: user.user
     }
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        UiActions: bindActionCreators(UiActionsCreator, dispatch)
+        UiActions: bindActionCreators(UiActionsCreator, dispatch),
+        UserActions: bindActionCreators(UserActionsCreator, dispatch)
     }
 };
 
-const Header = (props) => {
-    const toogleSidebar = () => {
-        props.UiActions.toogleSidebar();
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
     };
 
-    const toogleSearchbar = () => {
-        props.UiActions.toogleSearchbar();
+    componentDidMount() {
+        //this.props.UserActions.fetchUser();
     };
 
-    return (
-        <nav className="Header u-shadow">
-            <div className="Header-menu-container">
-                <button className="btn Header-menu" onClick={toogleSidebar}>
-                    <i className="fa fa-bars"></i>
-                </button>
-            </div>
-            <div className="Header-title">
-                <h3>{props.appName}</h3>
-            </div>
-            <div className="Header-search-container">
-                <button className="btn Header-search" onClick={toogleSearchbar}>
-                    <i className="fa fa-search"></i>
-                </button>
-            </div>
-            <div className="Header-datepicker-container">
-                <DatePicker />
-            </div>
-        </nav>
-    );
-};
+    componentWillUpdate() {
+        //console.log('componentWillUpdate', this.props);
+    };
+
+    componentDidUpdate() {
+        //console.log('componentDidUpdate', this.props);
+    }
+
+    toogleSidebar() {
+        this.props.UiActions.toogleSidebar();
+    };
+
+    render () {
+        return (
+            <nav className="Header u-shadow">
+                <div className="Header-menu-container">
+                    <button className="btn Header-menu" onClick={this.toogleSidebar.bind(this)}>
+                        <i className="fa fa-bars"></i>
+                    </button>
+                </div>
+                <Link to="/" className="Header-title">
+                    <h3>{this.props.appName}</h3>
+                </Link>
+                <div className="Header-datepicker-container">
+                    <DatePicker />
+                </div>
+            </nav>
+        );
+    }
+}
 
 Header.propTypes = {
     appName: React.PropTypes.string
