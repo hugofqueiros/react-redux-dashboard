0a2f1f3317d90cbd8137d25c5ebfd4800b27a2d5
Hugo Queiros <hugofqueiros@gmail.com>
2017-01-30 20:54:35 +0100

    feat(redux): store: crashLogMiddleware

---
+++ src/js/redux/store/crashLogMiddleware.js
@@ -0,0 +1,28 @@
+import Raven from 'raven-js';
+import Config from '../../config';
+
+Raven.config(Config.sentry.endpoint).install();
+
+export const logger = (store) => next => action => {
+    console.log('dispatching: ', action);
+    let result = next(action);
+
+    console.log('next state :', store.getState());
+
+    return result;
+};
+
+export const crashReporter = (store) => next => action => {
+    try {
+        return next(action);
+    } catch (err) {
+        console.error('Caught an exception', err);
+        Raven.captureException(err, {
+            extra: {
+                action,
+                state: store.getState()
+            }
+        });
+        throw err;
+    }
+};
