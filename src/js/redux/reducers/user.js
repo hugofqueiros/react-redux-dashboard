4b6d8b07171f1cda6c048279a2ed71b2905f2ee1
Hugo Queiros <hugofqueiros@gmail.com>
2017-01-30 20:51:11 +0100

    feat(redux): reducers: user

---
+++ src/js/redux/reducers/user.js
@@ -0,0 +1,36 @@
+import actionType from '../actions/actionTypes';
+
+const defaultState = {
+    data: {},
+    error: null,
+    isFetching: false
+};
+
+export default (state = defaultState, action) => {
+    switch (action.type) {
+        case actionType.FETCH_USER:
+            return {
+                ...state,
+                isFetching: true
+            };
+            break;
+        case actionType.FETCH_USER_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                data: action.payload.data,
+                error: null
+            };
+            break;
+        case actionType.FETCH_USER_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                data: {},
+                error: action.payload
+            };
+            break;
+    }
+
+    return state;
+};
