8e1fd388bdf1669b3806116d433bb5f12b1665fd
Hugo Queiros <hugofqueiros@gmail.com>
2017-02-07 20:29:36 +0100

    feat(components): new clock component

---
+++ src/js/components/clock/Clock.js
@@ -0,0 +1,50 @@
+import React, { PropTypes } from 'react';
+import moment from 'moment';
+
+import './Clock.scss';
+
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+        this.setState({
+            time: moment()
+        })
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => {
+            this.setState({
+                time: moment()
+            })
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    toogleDatePicker() {
+
+    }
+
+    render() {
+        let time = null;
+        if(this.state) {
+            time = this.state.time.format('YYYY-MM-DD HH:mm:ss');
+        }
+
+        return (
+            <div className="Clock">
+                {time}
+            </div>
+        )
+    }
+}
+
+export default Clock;
+
+
